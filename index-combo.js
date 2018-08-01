d313b5279fd05dfef03aa8c5c293a9956f9e67b5
Charles Villa <charlesvilla@outlook.com>
2018-07-31 18:21:24 -0700

    basic functionality working

--- index-combo.js
+++ index-combo.js
@@ -9,7 +9,7 @@ function getDataFromApi(searchTerm, callback) {
   const settings = {
     url: MA_SEARCH_URL,
     data: {
-      dateRange: `${searchTerm}`,
+      dateRange: '1991-11-01,1991-12-01',//`${searchTerm}`,
       ts: 1,
       apikey: PUB_KEY,
       hash: PRI_KEY,
@@ -50,26 +50,29 @@ function getEbayDataFromApi(title, callback) {
 
 var ebayTitle = {};
 var errTitles = [];
-
+var comicNames = {}
 function renderResult(listed) {
   var randNum = Math.floor(Math.random() * 1000);
-  ebayTitle.sea = `${listed.title}`
-  console.log(ebayTitle.sea);
-  ebayTitle.sea = "marvSearch" + ebayTitle.sea.replace(/\s/g,'').slice(0, -10) + randNum;
+  ebayTitle.sea = listed.title;
+  console.log(ebayTitle);
+  ebayTitle.sea = ebayTitle.sea.replace(/\s/g, '').slice(0, -10) + randNum;
+  ebayTitle.div = "div" + ebayTitle.sea;
+  ebayTitle.hee = "h2" + ebayTitle.sea;
+  comicNames[ebayTitle.sea] = listed.title;
   errTitles.push(ebayTitle.sea);
   console.log(ebayTitle.sea);
   return `
     <div>
-      <h2>
-      <a class="js-result-name" href="${listed.urls[0].url}" target="_blank">${listed.title}<br>
-      <img src="${listed.thumbnail.path}/portrait_uncanny.jpg"></a></h2>
-      <button id="${ebayTitle.sea}" class="js-ebay-search">Find Items on Ebay</button>
-      <div class="js-ebay-results"></div>
+    <h2 id=${ebayTitle.hee}>
+    <a class="js-result-name" href="${listed.urls[0].url}" target="_blank"><span>${listed.title}</span><br>
+    <img src="${listed.thumbnail.path}/portrait_uncanny.jpg"></a></h2>
     </div>
-  `;
+    <button id="${ebayTitle.sea}" class="js-ebay-search">Find Items on Ebay</button>
+    <div id=${ebayTitle.div} class="js-ebay-results"></div>
+`;
 }
 
-function renderEbayResult(ebdata){
+function renderEbayResult(ebdata) {
   console.log(ebdata);
   return `
   <p>HOLDER:${ebdata}</p>
@@ -92,7 +95,7 @@ function watchSubmit() {
     console.log(autoCompleteStart)
     const autoCompleteEnd = $('.date2').val();
     console.log(autoCompleteEnd)
-    const query = autoCompleteStart+","+autoCompleteEnd;
+    const query = autoCompleteStart + "," + autoCompleteEnd;
     // const queryTarget = $(event.currentTarget).find('.js-query');
     // const query = queryTarget.val();
     // clear out the input
@@ -110,31 +113,35 @@ function displayMarvelSearchData(data) {
 }
 
 
-function displayMarvelTitle(data, renderResult){
+function displayMarvelTitle(data, renderResult) {
   console.log(data);
   console.log(errTitles);
-  let marvTitle = data.data.results[0].title;
-  $("#" + errTitles).on('click', event => {
+  $('button').on('click', event => {
+    let marvTitle = comicNames[event.currentTarget.id];
     event.preventDefault();
     console.log("what up");
     console.log(event.currentTarget.id);
+    trouble = event.currentTarget.id
     console.log(marvTitle);
-  getEbayDataFromApi(marvTitle, displayEbaySearchData);
-})
+    getEbayDataFromApi(marvTitle, displayEbaySearchData);
+  })
 }
 
-function marvelParser(data){
+
+
+function marvelParser(data) {
   displayMarvelSearchData(data)
   displayMarvelTitle(data)
 }
 
 function displayEbaySearchData(data, target) {
   console.log("I EXIST");
+  console.log(trouble);
   data = JSON.parse(data);
   data = data.findItemsAdvancedResponse[0].searchResult[0];
   let listingDisplay = data.item[0].title.map((item, index) => renderEbayResult(item));
   console.log("hi" + listingDisplay);
-  $('.js-ebay-results').html(listingDisplay.join());
+  $('#' + 'div' + trouble).html(listingDisplay.join());
 }
 
 
