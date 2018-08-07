c6aa02c5716e9f87e471b8d96fd769358ef9c1c6
Charles Villa <charlesvilla@outlook.com>
2018-08-06 18:16:14 -0700

    updated responsiveness/accessibility

--- index-combo.js
+++ index-combo.js
@@ -62,24 +62,24 @@ function renderResult(listed) {
   let randNum = Math.floor(Math.random() * 1000);
   ebayTitle.sea = listed.title;
   ebayTitle.eba = listed.title.replace(/[(#)]/g, '');
-  ebayTitle.sea = ebayTitle.sea.replace(/[{(.:/,\s)}]/g, '').slice(0, -11) + randNum;
+  ebayTitle.sea = ebayTitle.sea.replace(/[{(.:/,'#\s)}]/g, '').slice(0, -11) + randNum;
   ebayTitle.div = "div" + ebayTitle.sea;
   comicNames[ebayTitle.sea] = listed.title;
   return `
     <div class="col-4">
       <div class="listing-image"><a href="${listed.urls[0].url}" target="_blank">
-        <img src="${listed.thumbnail.path}/portrait_uncanny.jpg" alt="${listed.title}"></a>
+        <img src="${listed.thumbnail.path}/portrait_uncanny.jpg" class="marv-pic" alt="${listed.title}"></a>
       </div>
       <div class="listing-title"><h2 class="listing title">
         <a href="${listed.urls[0].url}" target="_blank">${listed.title}</a></h2>
       </div>
       <div class="eb-button">
-        <button id="${ebayTitle.sea}" class="ebayButton" href=#${ebayTitle.div} data-lity>Find Issue on Ebay</button>
+        <button href=#${ebayTitle.div} id="${ebayTitle.sea}" class="ebayButton" role="button"  data-lity>Find Issue on Ebay</button>
       </div>
-      <div id=${ebayTitle.div} class="js-ebay-results col-12 lity-hide">
+      <div id=${ebayTitle.div} role="document" class="js-ebay-results col-12 lity-hide" aria-hidden="true">
         <div class="eb-error hidden"><p>SORRY NO RESULTS</p>
           <a href="https://www.ebay.com/sch/i.html?_from=R40&_trksid=m570.l1313&_nkw=${ebayTitle.eba}&_sacat=63" target="_blank">
-          Check eBay for similar titles</a>
+          <u>Check eBay for similar titles</u></a>
           <div class="eb-browse"></div>
         </div>
       </div>
@@ -88,9 +88,9 @@ function renderResult(listed) {
 }
 
 function renderEbayResult(ebdata) {
-  console.log(ebdata);
+  $(this).parent().attr('aria-hidden="true"')
   return `
-  <div class="eb-light">
+  <div class="eb-light" role="document" aria-live="assertive">
     <a href="${ebdata.viewItemURL[0]}" class="eb-results" target="_blank">
     <img class="eb-pic" src="${ebdata.pictureURLSuperSize}" alt="${ebdata.title}">
     <p class="eb-title">${ebdata.title}</p></a>
@@ -101,16 +101,21 @@ function renderEbayResult(ebdata) {
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
-    const autoCompleteStart = $('#date1').val();
-    const autoCompleteEnd = $('#date2').val();
-    const query = autoCompleteStart + "," + autoCompleteEnd;
+    const dateStart = $('#date1').val();
+    const dateEnd = $('#date2').val();
+    const query = dateStart + "," + dateEnd;
     page.searchTerm = query;
     console.log(page.searchTerm);
-    // $('.js-search-display').html(showSearchTerm);
     $('.js-search-results').empty();
     getDataFromApi(query, marvelParser);
   });
 }
+
+function showInstructions() {
+  $('#howdo').on('click', event => {
+    $('#instructions').removeClass('hidden').attr("aria-hidden", "false");
+  })
+};
 
 function displayMarvelSearchData(data) {
   const characterDisplay = data.data.results.map((item, index) => renderResult(item));
@@ -124,6 +129,7 @@ function displayMarvelTitle(data, renderResult) {
     let marvTitle = comicNames[event.currentTarget.id];
     trouble = event.currentTarget.id;
     getEbayDataFromApi(marvTitle, displayEbaySearchData);
+    $('.js-ebay-results').attr("aria-hidden", "false");
   });
 }
 
@@ -133,7 +139,6 @@ function marvelParser(data) {
 }
 
 function displayEbaySearchData(data, target) {
-  console.log("I EXIST");
   data = data.findItemsAdvancedResponse[0].searchResult[0];
   let listingDisplay = [];
   if (data.item) {
@@ -153,6 +158,7 @@ $(document).ready(function() {
   });
   $(document).on('lity:close', function(event, instance) {
     $('.eb-error').addClass('hidden');
+    $('#instructions').addClass('hidden');
   });
   $('#submit').click(function() {
     $("#txt").load("demo_ajax_load.asp");
@@ -162,3 +168,4 @@ $(document).ready(function() {
 
 
 $(watchSubmit);
+$(showInstructions);
